be794bbcabea97c147c2f7ae2f7212a1db56704b
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-01 19:13:18 -0400

    add function to get organization id and a function to update account in db

--- db/queries/users.js
+++ db/queries/users.js
@@ -7,6 +7,8 @@ const getUsers = () => {
     });
 };
 
+//// fix the org id issue
+// takes in the information from the create account form as an object and updates db
 const addAccount = function(account) {
   const queryStr = `INSERT INTO accounts (
     username, password, website_name, website_url, organization_id, category_id)
@@ -22,6 +24,7 @@ const addAccount = function(account) {
       console.log(err.message);
     });
 };
+
 
 const organizationName = function(userId) {
   const queryStr = `
@@ -42,4 +45,65 @@ const organizationName = function(userId) {
 };
 
 
-module.exports = { getUsers, addAccount, organizationName};
+const organizationId = function(userId) {
+  const queryStr = `
+  SELECT organization_id
+  FROM users
+  WHERE users.id = $1;
+    `;
+  const queryArgs = [userId];
+  return db.query(queryStr, queryArgs)
+    .then((results) => {
+      console.log("org id", results.rows);
+      return results.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+
+
+// takes in the information from the update account form as an object and updates db
+const updateAccount = function(account, accountId) {
+  const queryStr = `
+  UPDATE accounts
+  SET username = $1,
+  password = $2,
+  website_name = $3,
+  website_url = $4,
+  category_id = $5
+  WHERE id = $6;
+    `;
+  const queryArgs = [account.username, account.password, account.websiteName, account.websiteUrl, account.categoryId, accountId];
+  return db.query(queryStr, queryArgs)
+    .then((results) => {
+      console.log(results.rows);
+      return results.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+
+
+
+
+
+module.exports = { getUsers, addAccount, organizationName, organizationId, updateAccount};
+
+
+const account = {
+  username: 'Ally123',
+  password: 'Password!',
+  websiteName: 'amazon',
+  websiteUrl: 'https://www.amazon.ca',
+  categoryId: 12
+};
+
+updateAccount(account, 1);
+
+
+
+
