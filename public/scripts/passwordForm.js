c8851e8f2fc3b7e21b2c67dcf54c0220fa718b66
creynolds8 <cameron8.cr@gmail.com>
2024-06-02 11:22:03 -0700

    create event listener to call function that gets password form values

---
+++ public/scripts/passwordForm.js
@@ -0,0 +1,16 @@
+const { generatePassword } = require('randompassword.js')
+
+  document.getElementById('password-generator').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const passwordOptions = {
+      length: document.getElementById('length').value,
+      lowercase: document.getElementById('lower-case').checked,
+      uppercase: document.getElementById('upper-case').checked,
+      numbers: document.getElementById('numbers').checked,
+      specialChars: document.getElementById('special-chars').checked,
+    };
+    console.log(passwordOptions);
+    const password = generatePassword(passwordOptions)
+    console.log(password);
+  })
+
