758f0ffde3afcc5d6b8023bf6f11d8c8002d9190
creynolds8 <cameron8.cr@gmail.com>
2024-06-02 11:54:17 -0700

    import randompassword function and auto format, console is logging expected output

--- public/scripts/passwordForm.js
+++ public/scripts/passwordForm.js
@@ -1,16 +1,23 @@
-const { generatePassword } = require('randompassword.js')
+import { generatePassword } from "./randompassword.js";
 
-  document.getElementById('password-generator').addEventListener('submit', function(event) {
+document
+  .getElementById("password-generator")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
     const passwordOptions = {
-      length: document.getElementById('length').value,
-      lowercase: document.getElementById('lower-case').checked,
-      uppercase: document.getElementById('upper-case').checked,
-      numbers: document.getElementById('numbers').checked,
-      specialChars: document.getElementById('special-chars').checked,
+      length: document.getElementById("length").value,
+      lowercase: document.getElementById("lower-case").checked,
+      uppercase: document.getElementById("upper-case").checked,
+      numbers: document.getElementById("numbers").checked,
+      specialChars: document.getElementById("special-chars").checked,
     };
     console.log(passwordOptions);
-    const password = generatePassword(passwordOptions)
+    const password = generatePassword(
+      passwordOptions.length,
+      passwordOptions.lowercase,
+      passwordOptions.uppercase,
+      passwordOptions.specialChars,
+      passwordOptions.numbers
+    );
     console.log(password);
-  })
-
+  });
