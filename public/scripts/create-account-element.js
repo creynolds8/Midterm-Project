f56b59130fc1474ee251f77c96f9883d530fb971
creynolds8 <cameron8.cr@gmail.com>
2024-06-03 09:59:52 -0700

    add jquery with html framework to createAccountElement and export both functions

--- public/scripts/create-account-element.js
+++ public/scripts/create-account-element.js
@@ -1,11 +1,22 @@
 const createAccountElement = function (account) {
-
-}
-
+  const newAccount = $(`
+  <article>
+  <p>${account.website_name}</p>
+  <p>${account.username}</p>
+  <button class="account-button">Copy username</button>
+  <p>${account.password}</p>
+  <button class="account-button">Copy password</button>
+  <button class="account-button">Edit</button>
+  </article>
+  `);
+  return newAccount;
+};
 
 const renderAccounts = function (accounts) {
   for (const account of accounts) {
     const $account = createAccountElement(account);
-    $('.accounts').prepend($account);
+    $(".accounts").prepend($account);
   }
 };
+
+module.exports = { createAccountElement, renderAccounts };
