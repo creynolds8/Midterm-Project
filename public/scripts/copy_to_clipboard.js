2ac944f2fdb031cdaae37507f25f76c0fe267106
Gokul Krishna <gokursx.123@gmail.com>
2024-06-03 14:18:57 -0700

    Add copy_to_clipboard.js file to implement event listener

--- public/scripts/copy_to_clipboard.js
+++ public/scripts/copy_to_clipboard.js
@@ -2,17 +2,8 @@
 
 import { copyText } from "./copytext.js";
 
-document.getElementById("username")
-.addEventListener("copy", function (event) {
-  event.preventDefault();
-  console.log(copyText);
-  const copyToClipboard = copyText();
-  console.log(copyToClipboard);
-  $('#clipboard-text').val(copyToClipboard);
-});
-
-document.getElementById("password")
-.addEventListener("copy", function (event) {
+document.getElementsByClassName("account-button")
+.addEventListener("submit", function (event) {
   event.preventDefault();
   console.log(copyText);
   const copyToClipboard = copyText();
