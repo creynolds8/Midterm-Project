708075f7cafbb40fc8bbce476eaa2684c3770462
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-03 15:20:01 -0400

    'add query that gets all the accounts from the organization associated with the user which belong to a specified category'

--- db/queries/06_get_accounts_by_category.sql
+++ db/queries/06_get_accounts_by_category.sql
@@ -3,28 +3,6 @@
   JOIN categories ON categories.id = category_id
   JOIN organizations ON category_id = categories.id
   JOIN users ON organizations.id = users.organization_id
-  WHERE categories.id = "" AND users.organization_id = (SELECT organization_id
+  WHERE categories.id = 1 AND accounts.organization_id = (SELECT organization_id
     FROM users
-    WHERE users.id = 2);
-
-
--- const getAccountsByCategory = function(userId) {
---   const queryStr = `
---   SELECT DISTINCT accounts.*
---   FROM accounts
---   JOIN categories ON categories.id = category_id
---   JOIN organizations ON category_id = categories.id
---   JOIN users ON organizations.id = users.organization_id
---   WHERE users.organization_id = (SELECT organization_id
---     FROM users
---     WHERE users.id = $1;);
---     `;
---   const queryArgs = [userId];
---   return db.query(queryStr, queryArgs)
---     .then((results) => {
---       return results.rows;
---     })
---     .catch((err) => {
---       console.log(err.message);
---     });
--- };
+    WHERE users.id = 1);
