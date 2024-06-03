12b8cefd157e1c876a0e5043e5cc555c5f536e67
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-03 19:00:30 -0400

    'fix typo in get accounts by category id'

--- db/queries/06_get_accounts_by_category.sql
+++ db/queries/06_get_accounts_by_category.sql
@@ -1,7 +1,7 @@
-  SELECT DISTINCT accounts.*
+    SELECT DISTINCT accounts.*
   FROM accounts
   JOIN categories ON categories.id = category_id
-  JOIN organizations ON category_id = categories.id
+  JOIN organizations ON organization_id = organization_id
   JOIN users ON organizations.id = users.organization_id
   WHERE categories.id = 1 AND accounts.organization_id = (SELECT organization_id
     FROM users
