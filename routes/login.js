fc5837e6955600c8b5610ff81f1807506ff6426e
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-04 17:04:22 -0400

    'add users name and organizations name to cookie'

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const { organizationName, getUserName} = require('../db/database');
 const router  = express.Router();
 
 
@@ -7,9 +8,21 @@ const router  = express.Router();
 router.get('/:id', (req, res) => {
   req.session.userId = req.params.id;
   console.log("user id", req.session.userId);
-  res.redirect('/accounts');
+  organizationName(req.session.userId)
+    .then((orgName) => {
+      req.session.organizationName = orgName["organization_name"];
+      console.log("cookie orgName: ", req.session.organizationName);
+      return getUserName(req.session.userId);
+    })
+    .then((userName) => {
+      req.session.userName = userName.name;
+      console.log("cookie userName: ", req.session.userName);
+      res.redirect('/accounts');
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 });
-
 
 module.exports = router;
 
