977306607734d4cf3bb18c86bd98ff94c35e8768
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-04 11:48:53 -0400

    'update the account/:id get and post routes to render the page with the form perfilled with the appropriate values  and change the values in the db when the button it pushed'

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,20 +1,46 @@
 const express = require('express');
-const router  = express.Router();
-const { allAccounts } = require('../db/database');
+const router = express.Router();
+const { allAccounts, getAccountById, updateAccount } = require('../db/database');
 
 router.get('/getaccounts', (req, res) => {
   allAccounts(req.session.userId)
-  .then((response) => {
-    res.json(response);
-  })
+    .then((response) => {
+      res.json(response);
+    });
 });
 
 router.get('/', (req, res) => {
-    res.render('accounts');
+  res.render('accounts');
 });
 
 router.get('/:id', (req, res) => {
-  res.render('edit-account');
+  const accountId = req.params.id;
+  console.log("accountId", accountId);
+  getAccountById(accountId)
+    .then((results) => {
+      console.log("results", results);
+      const categoryValue = results["category_id"]
+      const templateVars = { account: results, userId: req.session.userId };
+      console.log("template Vars", templateVars);
+      res.render('edit-account', templateVars);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
 });
+
+router.post('/:id', (req, res) => {
+  const account = req.body;
+  console.log("account", account);
+  updateAccount(account)
+    .then(() => {
+      res.redirect('/accounts');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
 
 module.exports = router;
