cbcf9da1a4c5a7c62ef8fa89dd8acb5a0d55e0de
Gokul Krishna <gokursx.123@gmail.com>
2024-06-05 11:27:46 -0700

    Add new users into table

--- db/seeds/01_organizations.sql
+++ db/seeds/01_organizations.sql
@@ -1,6 +1,8 @@
 -- organizations table seeds here
 INSERT INTO organizations (name) VALUES ('Zenith Innovations');
 INSERT INTO organizations (name) VALUES ('Vertex Ventures');
-
+INSERT INTO organizations (name) VALUES ('Optimus Solutions');
+INSERT INTO organizations (name) VALUES ('Prime Consultants');
+INSERT INTO organizations (name) VALUES ('Selena Corp');
 
 
--- db/seeds/02_users.sql
+++ db/seeds/02_users.sql
@@ -2,3 +2,6 @@
 
 INSERT INTO users (name, password, email, organization_id) VALUES ('Alice', 'P@ssw0rd123', 'alice@example.com', 1);
 INSERT INTO users (name, password, email, organization_id) VALUES ('Bob', 'SecurePass456', 'bob@example.org', 2);
+INSERT INTO users (name, password, email, organization_id) VALUES ('Ray', 'SecurePass789', 'ray@example.org', 3);
+INSERT INTO users (name, password, email, organization_id) VALUES ('Diachi', 'SecurePass987', 'diachi@example.org', 4);
+INSERT INTO users (name, password, email, organization_id) VALUES ('Lucas', 'SecurePass654', 'lucas@example.org', 5);
