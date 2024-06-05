abb6b13b10951dabca69077ad7d979bff16a39ec
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-05 19:16:52 -0400

    'add error handling'

--- public/scripts/error-handling.js
+++ public/scripts/error-handling.js
@@ -1,12 +1,12 @@
 
 
 $(document).ready(function () {
-  $('#edit-button').click(function(event) {
+  $('#edit-button').click(function (event) {
 
     // Check if fields are empty
-    var website = $('#website-input').val().trim();
-    var username = $('#username-input').val().trim();
-    var password = $('#password-input').val().trim();
+    const website = $('#website-input').val().trim();
+    const username = $('#username-input').val().trim();
+    const password = $('#password-input').val().trim();
 
     if (website === '' || username === '' || password === '') {
       event.preventDefault();
@@ -17,4 +17,40 @@ $(document).ready(function () {
   });
 });
 
+$(document).ready(function () {
+  $('#creatAccount').click(function (event) {
+
+    // Check if fields are empty
+    const website = $('#website-input').val().trim();
+    const username = $('#username-input').val().trim();
+    const password = $('#password-input').val().trim();
+
+    if (website === '' || username === '' || password === '') {
+      event.preventDefault();
+      $('div.error-message').text('Error: Oops! Did you forget to type something? Please fill in all fields and try again :)').slideDown();
+      return;
+    }
+    $('div.error-message').slideUp();
+  });
+});
+
+
+
+
+
+
+
+// $(document).ready(function () {
+//   const handleFormSubmission = function() {
+//     const website = $('#website-input').val().trim();
+//     const username = $('#username-input').val().trim();
+//     const password = $('#password-input').val().trim();
+
+//     if (website === '' || username === '' || password === '') {
+//       $('div.error-message').text('Error: Oops! Did you forget to type something? Please fill in all fields and try again :)').slideDown();
+//       return false; // Prevent form submission
+//     }
+//     $('div.error-message').slideUp();
+//   }
+// });
 
