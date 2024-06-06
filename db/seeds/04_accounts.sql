4037d2c8ed9ce2949951ff7dbf486b7380316cae
Gokul Krishna <gokursx.123@gmail.com>
2024-06-06 11:48:33 -0700

    insert values to accounts table

--- db/seeds/04_accounts.sql
+++ db/seeds/04_accounts.sql
@@ -9,33 +9,44 @@ INSERT INTO accounts (username, password, website_name, website_url, organizatio
 INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('socialuser6', 'socialpass321', 'Quora','https://www.quora.com/',4,1);
 INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('socialuser6', 'socialpass321', 'Tumblr','https://www.tumbler.com/',5,1);
 
+
 -- Financial
 INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('finuser1', 'password123', 'PayPal', 'https://www.paypal.com/', 1, 2);
 INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('finuser2', 'securepass456', 'QuickBooks Online', 'https://quickbooks.intuit.com/', 1, 2);
 INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('finuser3', 'securepass789', 'Stripe','https://www.stripe.com/',3,2);
-INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('finuser4', 'securepass123', 'Venmogit','https://www.tumbler.com/',3,2);
+INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('finuser4', 'securepass123', 'Venmogit','https://www.venmogit.com/',4,2);
 
 -- Email
 INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('emailuser1', 'emailpass123', 'Gmail', 'https://mail.google.com/', 1, 3);
 INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('emailuser2', 'securemail789', 'Outlook', 'https://outlook.live.com/', 1, 3);
-INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('emailuser3', 'securemail456', 'Hotmail', 'https://outlook.live.com/', 3, 3);
+INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('emailuser3', 'securemail456', 'Hotmail', 'https://hotmail.com/', 3, 3);
+INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('emailuser3', 'securemail456', 'Protonmail', 'https://proton.me/', 4, 3);
 
 -- Cloud Storage
 INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('clouduser1', 'cloudpass123', 'Google Drive', 'https://drive.google.com/', 1, 4);
 INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('clouduser2', 'securecloud456', 'Dropbox', 'https://www.dropbox.com/', 1, 4);
 INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('clouduser3', 'securecloud456', 'Pcloud', 'https://www.pcloud.com/', 3, 4);
+INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('clouduser3', 'securecloud456', 'Hubspot', 'https://www.hubspot.com/', 4, 4);
+INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('clouduser3', 'securecloud456', 'Adobe', 'https://www.adobe.com/ca/creativecloud.html', 5, 4);
 
 -- Project Management
 INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('projuser1', 'projpass123', 'Trello', 'https://trello.com/', 1, 5);
 INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('projuser2', 'secureproj456', 'Asana', 'https://asana.com/', 1, 5);
 INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('projuser3', 'secureproj456', 'Hive', 'https://hive.com/', 3, 5);
+INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('projuser3', 'secureproj456', 'Jira', 'https://jira.com/', 4, 5);
+INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('projuser3', 'secureproj456', 'Zoho', 'https://zoho.com/', 5, 5);
 
 -- Product Management
 INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('produser1', 'prodpass123', 'Shopify', 'https://www.shopify.com/', 1, 6);
 INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('produser2', 'secureprod456', 'BigCommerce', 'https://www.bigcommerce.com/', 1, 6);
 INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('produser3', 'secureprod456', 'SquareSpace', 'https://www.squarespace.com/', 3, 6);
+INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('produser3', 'secureprod456', 'Wordpress', 'https://www.wordpress.com/', 4, 6);
+INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('produser3', 'secureprod456', 'Weebly', 'https://www.weebly.com/', 5, 6);
 
 -- Entertainment
 INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('entuser1', 'entpass123', 'Netflix', 'https://www.netflix.com/', 1, 7);
 INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('entuser2', 'secureent456', 'Spotify', 'https://www.spotify.com/', 1, 7);
-INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('entuser3', 'secureent456', 'Hulu', 'https://www.hulu.com/', 1, 7);
+INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('entuser3', 'secureent456', 'Hulu', 'https://www.hulu.com/', 3, 7);
+
+-- Security
+INSERT INTO accounts (username, password, website_name, website_url, organization_id, category_id) VALUES ('secuser3', 'secureent456', 'Norton', 'https://www.norton.com/', 3, 7);
